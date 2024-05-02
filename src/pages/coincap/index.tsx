e49b290f7f74fa12d768d9b4816b92c5fc76c5fe
Cherry <sharareshaddev@gmail.com>
2024-05-02 07:35:42 +0330

    improved handleMessage function , added data to the antd Table

--- src/pages/coincap/index.tsx
+++ src/pages/coincap/index.tsx
@@ -1,10 +1,58 @@
 import React, { useEffect, useState } from 'react';
 import WebSocketService from '../../services/WebsocketService';
+import Loading from '../../components/Loading';
+import { Table } from 'antd';
 
+interface Message {
+  bitcoin: string;
+  ethereum?: string;
+}
+interface CoinData {
+  name: string;
+  price: number | undefined; // Since prices can be undefined
+}
+const initialCoinData: CoinData[] = [
+  { name: 'bitcoin', price: undefined },
+  { name: 'ethereum', price: undefined },
+];
 const CoinCap: React.FC = () => {
-  const [messages, setMessages] = useState<string[]>([]);
+  const [coinData, setCoinData] = useState<CoinData[]>([]);
   const [isConnected, setIsConnected] = useState<boolean>(false);
 
+  const handleMessage = (message: string) => {
+    try {
+      const parsedData = JSON.parse(message);
+      if (!parsedData) {
+        return;
+      }
+
+      setCoinData((prevData) => {
+        if (prevData.length === 0) {
+          return initialCoinData;
+        }
+
+        return prevData.map((coin) => {
+          if (
+            coin.name === 'bitcoin' &&
+            typeof parsedData.bitcoin !== 'undefined'
+          ) {
+            return { ...coin, price: parsedData.bitcoin };
+          }
+
+          if (
+            coin.name === 'ethereum' &&
+            typeof parsedData.ethereum !== 'undefined'
+          ) {
+            return { ...coin, price: parsedData.ethereum };
+          }
+
+          return coin;
+        });
+      });
+    } catch (error) {
+      return error;
+    }
+  };
   useEffect(() => {
     const websocketUrl = import.meta.env.VITE_WEBSOCKET_URL;
 
@@ -15,10 +63,7 @@ const CoinCap: React.FC = () => {
       setIsConnected(true);
     });
 
-    wsService.onMessage((message) => {
-      console.log('message :>> ', message);
-      setMessages((prevMessages) => [...prevMessages, message]);
-    });
+    wsService.onMessage(handleMessage);
 
     wsService.onError((error) => {
       console.error('websocket error:', error.message);
@@ -34,17 +79,25 @@ const CoinCap: React.FC = () => {
       wsService.close();
     };
   }, []);
-
   return (
-    <div>
-      {!isConnected && 'loadig..'}
-      <ul>
-        {messages.map((msg, index) => (
-          <li key={index}>{msg}</li>
-        ))}
-      </ul>
+    <div className="w-full">
+      {isConnected && <Loading />}
+      <Table columns={columns} dataSource={coinData} rowKey="name" pagination={false} />{' '}
+      {/* Unique rowKey based on name */}
     </div>
   );
 };
 
 export default CoinCap;
+const columns = [
+  {
+    title: 'name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    key: 'price',
+    dataIndex: 'price',
+    title: 'price',
+  },
+];
