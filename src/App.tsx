2ea9a3a34dc4b498baaf43cea819fea4e9d2671f
Cherry <sharareshaddev@gmail.com>
2024-05-02 15:31:01 +0330

    feat:Implement Loading component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import { Suspense } from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import { RouteConfig, routes } from './routes';
 import Home from './pages/home';
+import Loading from './components/Loading';
 
 function App() {
   return (
     <Router>
-      <Suspense fallback={<>Loading...</>}>
+      <Suspense fallback={<Loading />}>
         <Routes>
           {routes.map((route: RouteConfig) => (
             <Route key={route.path} path={route.path} element={route.element} />
