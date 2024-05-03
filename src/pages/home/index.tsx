323b287342bb8530eedc7ec2b7078b2f2197dbf2
Cherry <sharareshaddev@gmail.com>
2024-05-03 09:55:51 +0330

    feat : Add Framer Motion for Smooth Transitions, Implement Glassmorphism Background, and Refine Home Page Styles

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,31 @@
 import { Link } from 'react-router-dom';
-import Layout from '../../components/Layout';
+import GlassMorphismBackground from '../../components/GlassMorphismBackground';
 
 function Home() {
   return (
-    <Layout>
-      <div>
-        <Link to={'/coincap'}>Coin Cap</Link>
+    <GlassMorphismBackground>
+      <div className="space-y-6">
+        <h1 className="text-3xl text-bold">Hello, Zarin Express!</h1>
+        <span className="text-xl">Welcome aboard to my project! 😊</span>
+        <p>
+          To see the tasks I've completed, please click on the links below.👇
+        </p>
       </div>
-    </Layout>
+      <div className="p-4 m-8 flex space-x-8">
+        <Link
+          to={'/coincap'}
+          className="border p-4 bg-blue-400 flex items-center rounded-md shadow-md text-white "
+        >
+          Coin Cap
+        </Link>
+        <Link
+          to={'/IMDB'}
+          className="border p-4 bg-blue-400 flex items-center rounded-md shadow-md  text-white"
+        >
+          IMDB
+        </Link>
+      </div>
+    </GlassMorphismBackground>
   );
 }
 
