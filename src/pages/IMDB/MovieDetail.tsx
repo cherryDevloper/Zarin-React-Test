2b9032fda3bf2fd46c35f6da0da39e461757d21f
Cherry <sharareshaddev@gmail.com>
2024-05-03 17:32:34 +0330

    feat:implement movie details using useLocation and state

---
+++ src/pages/IMDB/MovieDetail.tsx
@@ -0,0 +1,39 @@
+import { Button, Image, Typography } from 'antd';
+import { useLocation, useNavigate } from 'react-router-dom';
+import Layout from '../../components/Layout';
+
+const MovieDetail = () => {
+  const navigate = useNavigate();
+  const { state } = useLocation();
+  if (!state) navigate(-1);
+  const { movie } = state;
+  return (
+    <Layout>
+      <Button onClick={() => navigate(-1)}>{'<'} Back</Button>
+      <div className="my-10">
+        <Typography.Title>{movie.Title}</Typography.Title>
+        <div className="flex justify-between">
+          {' '}
+          <Image alt="" src={movie.Poster} className="rounded-md" />
+          <div className="flex  flex-col ml-4 space-y-4">
+            <Typography.Text>
+              <span className="text-bold text-yellow-500">Description:</span>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nam
+              nihil asperiores, eius, autem sint omnis quaerat id esse
+              voluptatem unde voluptatum quam natus perspiciatis obcaecati cum,
+              beatae a non.
+            </Typography.Text>
+
+            <Typography.Text>
+              {' '}
+              <span className="text-bold text-yellow-500">Data:</span>
+              {movie.Year}
+            </Typography.Text>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default MovieDetail;
